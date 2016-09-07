9a5e988823788d90afaf73a4b33a6a2fa8418af7
Sebastian Vieira <sebastian.vieira@homegate.ch>
2016-09-07 15:15:41 +0200

    downgrading annotations

--- gradle-android-cq-plugin/src/main/groovy/com/github/nrudenko/gradle/cq/AndroidCQPlugin.groovy
+++ gradle-android-cq-plugin/src/main/groovy/com/github/nrudenko/gradle/cq/AndroidCQPlugin.groovy
@@ -14,7 +14,7 @@ public class AndroidCQPlugin implements Plugin<Project> {
         project.configurations.create('findbugsPlugins')
         project.configurations.create('codequality')
 
-        project.dependencies.add('compile', 'com.google.code.findbugs:annotations:3.0.1')
+        project.dependencies.add('compile', 'com.google.code.findbugs:annotations:3.0.0')
         project.dependencies.add('findbugs', 'com.google.code.findbugs:findbugs-ant:3.0.0')
         project.dependencies.add('codequality', 'com.puppycrawl.tools:checkstyle:6.18')
         project.dependencies.add('codequality', 'pmd:pmd:4.3')
